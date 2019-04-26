52c7c3cd1ae98f849957de7c7a466a5a722463a9
ilankham <isaiah.lankham@gmail.com>
2019-04-26 09:14:57 -0700

    Add additional documentation to example SASPy config file

--- sascfg_personal-example.py
+++ sascfg_personal-example.py
@@ -9,28 +9,36 @@
 #
 #     C:\Program Files\SASHome\SASDeploymentManager\9.4\products\deploywiz__94492__prt__xx__sp0__1\deploywiz
 #
+#     However, depending on your SAS version (or maintenance release), the second-to-last portion
+#     deploywiz__94492__prt__xx__sp0__1 might differ.
+#
 # (3) Fill in the value of SASEXT_PATH below with the path to the directory containing the file
 #     sspiauth.dll file on your local machine. If default options were used when SAS was installed,
 #     this should be a path like the following:
 #
 #     C:\Program Files\SASHome\SASFoundation\9.4\core\sasext
 #
-# (4) Fill in the value of SITE_PACKAGES_PATH below with the path to the site directory for your
-#     Python installation. For example, if your project folder is at the root level of your C:\
-#     drive and you've created a virtual environment called venv, this should be a path like the
-#     following:
+# (4) Fill in the value of SITE_PACKAGES_PATH below with the path to the site-packages directory
+#     for a Python 3 installation with SASPy installed. For example, if your project folder is at
+#     the root level of your C:\ drive and you've created a virtual environment called venv, this
+#     should be a path like the following:
 #
 #     C:\dataset-explorer\venv\Lib\site-packages
 #
 # For more information, see https://sassoftware.github.io/saspy/install.html
+#
+# See also https://github.com/sassoftware/saspy/blob/master/saspy/sascfg.py for documentation of other
+# possible configuration options.
 
 
 import os
 
-
-JAVA_CLASSES_PATH = r'[FILL IN WITH YOUR LOCAL FILE PATH]'
-SASEXT_PATH = r'[FILL IN WITH YOUR LOCAL FILE PATH]'
-SITE_PACKAGES_PATH = r'[FILL IN WITH YOUR LOCAL FILE PATH]'
+# Fill in the paths below, and remove the square brackets. The r-prefix means "raw string",
+# allowing non-escaped back slashes to be used. (In other words, without the r-prefix, each
+# back slash would need to be doubled up, such as in 'C:\\Program Files\\SASHome\\'.)
+JAVA_CLASSES_PATH = r'[FILL IN WITH YOUR LOCAL FILE PATH AND REMOVE THE SQUARE BRACKETS]'
+SASEXT_PATH = r'[FILL IN WITH YOUR LOCAL FILE PATH AND REMOVE THE SQUARE BRACKETS]'
+SITE_PACKAGES_PATH = r'[FILL IN WITH YOUR LOCAL FILE PATH AND REMOVE THE SQUARE BRACKETS]'
 
 
 JAVA_CLASSES = [
